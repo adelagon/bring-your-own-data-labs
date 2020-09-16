cb3090a55bfd9cf2bb9449de3d6aad727df71a0a
Miguel <miguelmayor97@gmail.com>
2020-09-16 13:30:16 +0800

    updated JobDetailsPage

--- webtool/src/components/JobDetailPage.jsx
+++ webtool/src/components/JobDetailPage.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-import { API, graphqlOperation } from 'aws-amplify'
-import * as queries from '../graphql/queries'
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { getJobs as getJob } from '../graphql/queries';
 
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import TextField from '@material-ui/core/TextField'
 
-// const testJob = API.graphql(graphqlOperation(queries.getJobs, { id: 'test' }))
-// console.log(testJob)
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
 
 export default function JobDetailPage() {
+    const [field, setFields] = useState([]);
+
+    useEffect(() => {
+        getField()
+    }, []);
+
+    async function getField() {
+        try {
+            const getData = await API.graphql(graphqlOperation(getJob, {id: '88867fa8-698e-4fe6-9091-6efc3aa305cc'}));
+            const getField = getData.data.getJobs;
+            console.log(getField);
+            setFields(getField);
+        } catch (err) {
+            console.log("[ERROR] Fetching data: ", err)
+        }
+    }
+
     return (
         <div>
             <h3>Validation Job Status</h3>

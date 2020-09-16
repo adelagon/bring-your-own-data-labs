7b434f6147de720e93bcb5f81f5615e36b05367e
Miguel <miguelmayor97@gmail.com>
2020-09-16 10:02:48 +0800

    updated Splash Page

--- webtool/src/App.js
+++ webtool/src/App.js
@@ -33,40 +33,9 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div style={styles.container}>
-      <Router>
-        <List component="nav">
-          <ListItem  button component={Link} to="/">
-            <ListItemText primary="Splash Page" />
-          </ListItem>
-          <ListItem button component={Link} to="/jobs">
-            <ListItemText primary="Jobs List Page" />
-          </ListItem>
-          <ListItem button component={Link} to="/jobs/0eb09841-698b-4ba1-851c-f1a1c69a21e1">
-            <ListItemText primary="Job Detail Page" />
-          </ListItem>
-          <ListItem button component={Link} to="/options">
-            <ListItemText primary="Options Page" />
-          </ListItem>
-          <ListItem button component={Link} to="/options/cli">
-            <ListItemText primary="CLI Upload Page" />
-          </ListItem>
-          <ListItem button component={Link} to="/options/client">
-            <ListItemText primary="S3 Client Page" />
-          </ListItem>
-        </List>  
-        <Route exact path="/" component={SplashPage} />
-        <Route exact path="/jobs" component={JobsPage} />
-        <Route exact path="/jobs/:id" component={JobDetailPage} />
-        <Route exact path="/options" component={OptionsPage} />
-        <Route exact path="/options/cli" component={CLIOptionPage} />
-        <Route exact path="/options/client" component={S3ClientOptionPage} />
-      </Router>
-=======
     <div>
       <Container>
-        <Router>
+      <Router>
           <List component="nav">
             <Breadcrumbs aria-label="breadcrumb">
               <ListItem  button component={Link} to="/">
@@ -84,12 +53,11 @@ const App = () => {
               <ListItem button component={Link} to="/options/cli">
                 <ListItemText primary="CLI Upload Page" />
               </ListItem>
-              <ListItem button component={Link} to="/options/s3client">
+              <ListItem button component={Link} to="/options/client">
                 <ListItemText primary="S3 Client Page" />
               </ListItem>
-              </Breadcrumbs>
-          </List>
-          
+            </Breadcrumbs>
+          </List>  
           <Route exact path="/" component={SplashPage} />
           <Route exact path="/jobs" component={JobsPage} />
           <Route exact path="/jobs/:id" component={JobDetailPage} />
@@ -98,7 +66,6 @@ const App = () => {
           <Route exact path="/options/client" component={S3ClientOptionPage} />
         </Router>
       </Container>
->>>>>>> initial commit
     </div>
   )
 }

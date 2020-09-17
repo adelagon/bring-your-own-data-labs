4e1184b9140bdeb6ad931be307649227d28f168f
Alvin Delagon <adelagon@amazon.com>
2020-09-17 13:05:17 +0800

    Add stager lambda function

---
+++ webtool/amplify/backend/function/stager/src/index.js
@@ -0,0 +1,24 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: process.env['REGION']});
+s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+    // TODO implement
+
+    let source_object = process.env['SOURCE_BUCKET'] + '/' + event.source_object + '?versionId=' + event.source_version;
+    let dest_object = process.env['STAGE_BUCKET'] + '/' + event.source_object;
+
+    console.log(event, source_object, dest_object, event.source_object);
+    s3.copyObject({
+      CopySource: source_object,
+      Bucket: process.env['STAGE_BUCKET'],
+      Key: event.source_object
+    }, function(err, res) {
+      if (err) {
+        console.log('[ERROR]: ', err);
+      } else {
+        console.log('[SUCCESS]');
+      }
+    });
+    callback(null, 'DONE');
+};

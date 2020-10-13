01d7bd72422d0b8eeb3f9898ab39e1926691a598
Alvin Delagon <adelagon@amazon.com>
2020-10-13 10:03:55 +0800

    Reference Source S3 Bucket in Upload options pages

--- dvt/webtool/src/components/CLIOptionPage.jsx
+++ dvt/webtool/src/components/CLIOptionPage.jsx
@@ -7,6 +7,8 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
+import awsSDKExports from '../aws-sdk-exports';
+
 const useStyles = makeStyles((theme) => ({
     root: {
       '& > *': {
@@ -23,10 +25,10 @@ export default function CLIOptionPage() {
         <div>
             <Container>
                 <Typography variant="h6" id="tableTitle" component="div" align="left">Option 1: CLI Upload</Typography>
-                    <p>We have automatically created your S3 Bucket named: mhtan-byod-12345 in ap-southeast-1</p>
+                    <p>We have automatically created your S3 Bucket named: { awsSDKExports.source_s3_bucket } in ap-southeast-1</p>
                     <p>We also have created an IAM user named: ___________ that you may use for uploading data into your bucket</p>
                     <p>To upload your files through AWS CLI, all you have to do is provision the IAM credentials of the source machine and then run this command: </p>
-                    <Box border={1} px={1} py={1}>aws s3 cp {"<"}directory{">"} s3://mhtan-byod-12345/raw --recursive</Box>
+                    <Box border={1} px={1} py={1}>aws s3 cp {"<"}directory{">"} s3://{ awsSDKExports.source_s3_bucket }/raw --recursive</Box>
                     <p>For more details on how to upload files to S3 using the AWS CLI, you may visit: </p>
                     <p><a target="_blank" rel="noopener noreferrer" href="https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/index.html">https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/index.html</a></p>
                     <div className={classes.root}>
--- dvt/webtool/src/components/OptionsPage.jsx
+++ dvt/webtool/src/components/OptionsPage.jsx
@@ -5,13 +5,14 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import awsSDKExports from '../aws-sdk-exports';
 
 export default function OptionsPage() {
     return (
         <div>
         <Container>
             <Typography variant="h6" id="tableTitle" component="div" align="center">Upload Options</Typography>
-            <p>We have automatically created your S3 Bucket named: mhtan-byod-12345 in ap-southeast-1</p>
+            <p>We have automatically created your S3 Bucket named: { awsSDKExports.source_s3_bucket } in ap-southeast-1</p>
             <p>We also have created an IAM user named: ___________ that you may use for uploading data into your bucket</p>
             <p>You have the following options to upload your data into this bucket. Please click the following options that you want to explore: </p>
             <Box mx="auto" my={1}>

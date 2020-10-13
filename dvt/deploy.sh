7781b6c9acaa6769d7bf0e829478641f6d5fb1d2
Joben R <joben.rara@gmail.com>
2020-10-13 16:52:07 +0800

    removed isengard from requirements.txt; add --profile to aws cli calls

--- dvt/deploy.sh
+++ dvt/deploy.sh
@@ -9,21 +9,21 @@ echo 'Generating config file for frontend...'
 ### Dirty hack for now as CDK doesn't support returning outputs from app context
 #   We need these outputs to wire the frontend with
 printf "ClientId=" > outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='ClientId'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='ClientId'].OutputValue" --output=text >> outputs.txt
 printf "GraphQLEndpoint=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='GraphQLEndpoint'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='GraphQLEndpoint'].OutputValue" --output=text >> outputs.txt
 printf "UserPoolId=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='UserPoolId'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='UserPoolId'].OutputValue" --output=text >> outputs.txt
 printf "IdentityPoolId=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='IdentityPoolId'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='IdentityPoolId'].OutputValue" --output=text >> outputs.txt
 printf "SourceS3Bucket=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='SourceS3Bucket'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='SourceS3Bucket'].OutputValue" --output=text >> outputs.txt
 printf "TargetS3Bucket=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='TargetS3Bucket'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='TargetS3Bucket'].OutputValue" --output=text >> outputs.txt
 printf "SQSProfileQueue=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='SQSProfileQueue'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='SQSProfileQueue'].OutputValue" --output=text >> outputs.txt
 printf "StagerLambdaFunction=" >> outputs.txt
-aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='StagerLambdaFunction'].OutputValue" --output=text >> outputs.txt
+aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='StagerLambdaFunction'].OutputValue" --output=text >> outputs.txt
 mv outputs.txt webtool/src/
 cd webtool/src/
 ./generate_config.py
@@ -32,10 +32,11 @@ echo 'Building frontend...'
 npm run-script build
 
 echo 'Deploying frontend...'
-webtool_bucket=$(aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='WebToolS3Bucket'].OutputValue" --output=text)
+webtool_bucket=$(aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='WebToolS3Bucket'].OutputValue" --output=text)
 echo $webtool_bucket
-aws s3 sync ../build/ s3://$webtool_bucket
+aws s3 sync ../build/ s3://$webtool_bucket --profile=$1
 
-webtool_url=$(aws cloudformation describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='WebToolUrl'].OutputValue" --output=text)
+
+webtool_url=$(aws cloudformation --profile=$1 describe-stacks --stack-name byod-dvt --query="Stacks[0].Outputs[?OutputKey=='WebToolUrl'].OutputValue" --output=text)
 echo 'You may now open the webtool at:'
 echo $webtool_url
